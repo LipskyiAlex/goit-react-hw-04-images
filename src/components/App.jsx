1991cfaa36736c394eb960b85db610ef5083ca0a
Alex <alexlipskyi@icloud.com>
2023-09-26 22:59:22 +0300

    sds

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';  
+import React, {useState, useEffect} from 'react';  
 import css from './App.module.css';
 import 'react-toastify/dist/ReactToastify.css';
 
